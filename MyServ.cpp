51c7b095d23ffdc5d07af4bb116931078f41d52f
yuxing <376365042@qq.com>
2016-11-27 15:36:02 +0800

    改为线程池版本

--- MyServ.cpp
+++ MyServ.cpp
@@ -9,11 +9,15 @@
 #include <cstdlib>
 #include <cstdio>
 #include <ctype.h>
+#include <iostream>
 using namespace std;
 
 typedef const struct sockaddr CSA;
 typedef struct sockaddr SA;
 const int LISTENS_SIZE = 30;
+const int NTHREAD = 20;
+
+pthread_mutex_t mutex_listenfd = PTHREAD_MUTEX_INITIALIZER;
 
 #define SERVER_STRING "Server: httpd/0.1.0\r\n"
 
@@ -109,43 +113,47 @@ void send_homepage(int client)
 	write(1, "send ok\n", 8);
 }
 
-void* accept_request(void* client)
+void* accept_request(void* fd_listen)
 {
-	int fd_cli =*(int*) client;
-	delete (int*)client;
-	char buff[1048];
-	int n = getline(fd_cli, buff, sizeof(buff));
-	//write(1, buff, strlen(buff));write(1, "\n", 1);
-	char method[20];
-	sscanf(buff, "%s", method);
-	//write(1, method, strlen(method));
-	if (0 == strcmp(method, "GET"))
+	pthread_detach(pthread_self());
+	while (true)
 	{
-	   sscanf(buff+strlen(method), "%s", method);
-	   if (0 == strcmp(method, "/"))
-	   {
-			send_homepage(fd_cli);	   
-	   }   
+		pthread_mutex_lock(&mutex_listenfd);		
+		int fd_cli = accept(*(int*)fd_listen, NULL, NULL);
+		pthread_mutex_unlock(&mutex_listenfd);
+		
+		if (fd_cli < 0)
+			pthread_exit(NULL);
+
+		char buff[1048];
+		int n = getline(fd_cli, buff, sizeof(buff));
+		char method[20];
+		sscanf(buff, "%s", method);
+		if (0 == strcmp(method, "GET"))
+		{
+			sscanf(buff+strlen(method), "%s", method);
+			if (0 == strcmp(method, "/"))
+		   	{
+				send_homepage(fd_cli);	   
+			}   
+		}
+		close(fd_cli);  //
 	}
-	close(fd_cli);
-	return (void*)0;
+	pthread_exit(NULL);
 }
 
 int main()
 {
 	u_short port = 0;
-	int srv_sock = startup(&port);	
+	int fd_listen = startup(&port);	
 	printf("httpd running on port %d\n", port);
 	
-	for (;;)
+	for (int i = 0; i < NTHREAD; i++)
 	{
-		int* cli_sock = new int;
-		*cli_sock = accept(srv_sock, NULL, NULL);
-		if (-1 == *cli_sock)
-			error_die("accpet");
 		pthread_t newthread;
-		if (pthread_create(&newthread, NULL, accept_request, (void*)cli_sock) != 0)
-			error_die("pthread_create");
+		pthread_create(&newthread, NULL, accept_request, (void*)&fd_listen);			
 	}
-  	return 0;
+	while (true)
+		pause();
+	exit(0);
 }
